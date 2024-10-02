b4d9d3b73c5375ebffa0915103ed7f4bfd56736c
gerney <btcsideveloper@gmail.com>
2024-10-02 14:06:16 +0800

    Updated the 001_create_students.php migration file and added gender birthdate and contact number field

--- fuel/app/migrations/001_create_students.php
+++ fuel/app/migrations/001_create_students.php
@@ -13,6 +13,9 @@ class Create_students
 			'firstname' => array('constraint' => 150, 'null' => false, 'type' => 'varchar'),
 			'middlename' => array('constraint' => 150, 'null' => false, 'type' => 'varchar'),
 			'suffix' => array('constraint' => 50, 'null' => false, 'type' => 'varchar'),
+			'gender' => array('constraint' => 6, 'null' => false, 'type' => 'varchar'),
+			'birthdate' => array('constraint' => 10, 'null' => false, 'type' => 'varchar'),
+			'contact_no' => array('constraint' => 15, 'null' => false, 'type' => 'varchar'),
 			'created_at' => array('constraint' => '11', 'null' => false, 'type' => 'int'),
 			'updated_at' => array('constraint' => '11', 'null' => false, 'type' => 'int'),
 		), array('id'));
